538902808ba8fc64f1b14790bb1ab0c449b04a74
Robin Küster <robin_kuester@me.com>
2015-03-10 15:57:42 +0100

    The SizeUnits enum is now used for converting the units as a straing to the proper enum value.

--- src/main/java/com/guigarage/lessfx/converters/LessStyleConverter.java
+++ src/main/java/com/guigarage/lessfx/converters/LessStyleConverter.java
@@ -28,36 +28,10 @@ public abstract class LessStyleConverter<F, T> extends StyleConverter<F, T> {
     protected SizeUnits getUnit(String unit) {
         if (unit == null) {
             return null;
-        }
-        switch (unit.toLowerCase()) {
-            case "%":
-                return SizeUnits.PERCENT;
-            case "in":
-                return SizeUnits.IN;
-            case "cm":
-                return SizeUnits.CM;
-            case "mm":
-                return SizeUnits.MM;
-            case "em":
-                return SizeUnits.EM;
-            case "ex":
-                return SizeUnits.EX;
-            case "pt":
-                return SizeUnits.PT;
-            case "pc":
-                return SizeUnits.PC;
-            case "px":
-                return SizeUnits.PX;
-            case "grad":
-                return SizeUnits.GRAD;
-            case "deg":
-                return SizeUnits.DEG;
-            case "rad":
-                return SizeUnits.RAD;
-            case "turn":
-                return SizeUnits.TURN;
-            default:
-                return null;
+        } else if (unit.equals("")){
+            return null;
+        } else {
+            return SizeUnits.valueOf(unit.equals("%") ? "PERCENT" : unit.toUpperCase());
         }
     }
 
